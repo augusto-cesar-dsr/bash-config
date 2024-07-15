cac1e4616942ee97e6586502bc82ec089c8ed062
Augusto César <augusto_cesar.cc@hotmail.com>
2024-07-15 17:04:22 -0300

    add functions to autocompletion and to friendly use docker compose

--- bash_functions.sh
+++ bash_functions.sh
@@ -14,6 +14,7 @@ function termtitle() {
 # Take the branch in local folder, if have .git path
 parse_git_branch() { git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \ \1/' -e 's/[[:space:]]//g'; };
 
+########## Docker functions
 # return hostname, name and ips from cointaines running
 function docker-ips {
   local containers=$(docker container ls | grep -v 'CONTAINER'| awk '{ printf(" %s", $1) }')
@@ -22,9 +23,45 @@ function docker-ips {
   done
 }
 
-function project_path() {
-  PROJECT=$1
-  shift
-  cd "$PROJECTS_ROOT_PATH/$PROJECT/"
+# compose comands friendly from a compose file development
+function friendly-compose() {
+  # you need to use some standard like "docker-awesome-wonderful.yml" in your project
+  # then user the tag start or empty to see all tags from docker compose usage
+
+  docker compose -f $(find -name 'docker*-*-*.yml') $1
 }
 
+# starting bash docker function
+function open-bash-in-docker() {
+  docker exec -it $1 bash
+}
+
+# autocomplete to docker find name
+_docker_auto_completion_name() {
+  local name
+  for name in "$(docker ps --format {{.Names}} | grep -v 'NAMES' | grep -s -G "$2[a-z]*.*" )"; do
+    COMPREPLY+=( $(basename "$name") )
+  done
+}
+
+# link autocompletion in to function autocompletion
+complete -F _docker_auto_completion_name open-bash-in-docker
+
+############## to navigate
+
+# to navigate in custom projects paths
+function project_path() {
+  cd "$PROJECTS_ROOT_PATH"/$1
+}
+
+# funtion privated to path autocompletion
+_project_path_auto_complete() {
+    local file
+    for file in "$PROJECTS_ROOT_PATH/$2"*; do
+        [[ -d $file ]] || continue
+        COMPREPLY+=( $(basename "$file") )
+    done
+}
+
+# link autocompletion in to function autocompletion
+complete -F _project_path_auto_complete project_path
